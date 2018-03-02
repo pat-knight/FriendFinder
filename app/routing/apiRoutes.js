0f658a408c6be1e1b5339effbb59e0844d7e85b2
Patrick Knight <ptknight00@gmail.com>
2018-03-02 15:12:45 -0600

    api route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,27 @@
+var members = [];
+
+module.exports = function(app) {
+    //send JSON of all possible friends
+    app.get('/api/friends', (req, res) => {
+        res.json(members);
+    });
+
+    app.post('/data/friends', (req, res) => {
+        if (req.body) {
+            members.push(req.body);
+            findMatch();
+        }
+        
+        //handle incoming survey results and handle compatibility logic
+        var updateMembers = {
+            name: req.body.name,
+            photo: req.body.photo,
+            profile: req.body.profile
+        }
+        res.json(updatemembers);
+    });
+}
+
 app.get('/api/friends', (req, res) => {
     //display a JSON of all possible friends
 });
